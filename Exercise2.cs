605a0ec4a6f00f73bbd55e42f52513235c01e287
TATIANE <tatiane@tatianepedrelli.com.br>
2020-08-07 20:36:55 -0300

    fixed exercise 2

--- Exercise2.cs
+++ Exercise2.cs
@@ -5,13 +5,20 @@ namespace arrays2 {
     class ExerciseTwo {
         public static void Two(string[] args)
         {
-            Console.Write("Exercício 2.\nDado um vetor qualquer com 10 números,");
+            Console.Write("\nExercício 2.\nDado um vetor qualquer com 10 números,");
             Console.WriteLine("faça um programa que informa se há ou não números repetidos nesse vetor.");
 
-            int[] ages = new int[10] {28, 44, 43, 55, 10, 58, 67, 63, 10, 71};
+            const int arrayLenght = 10;
 
-            if (ages.Length != ages.Distinct().Count()) Console.WriteLine("Contém duplicatas.");
-            else Console.WriteLine("Não contém duplicatas.");
+            var x = new double[arrayLenght];
+
+            for (int i = 0; i < arrayLenght; i++) {
+                Console.WriteLine($"Informe o {i + 1} número");
+                x[i] = Double.Parse(Console.ReadLine());
+            }
+
+            if (x.Length != x.Distinct().Count()) Console.WriteLine("Este array contém números repetidos.");
+            else Console.WriteLine("Este array não contém números repetidos.");
 
         }
     }
