c0610dd2594dee854877447dc34ff69e6cf59709
TATIANE <tatiane@tatianepedrelli.com.br>
2020-08-07 20:22:22 -0300

    fixed exercise 1

--- Exercise1.cs
+++ Exercise1.cs
@@ -4,29 +4,49 @@ namespace arrays2 {
     class ExerciseOne {
         public static void One(string[] args)
         {
-            Console.WriteLine("Exercício 1.\nPopule dois vetores com 10 valores cada.");
+            Console.WriteLine("\nExercício 1.\nPopule dois vetores com 10 valores cada.");
             Console.WriteLine("Após esta operação troque o valor dos vetores.\n");
 
-            double[] a = new double[10] {7.0, 8.5, 9, 9.5, 8, 10, 7.8, 7.5, 9.5, 10};
-            double[] b = new double[10] {6.0, 6.5, 8, 7.5, 8, 8.5, 7.8, 7.5, 9, 9.5};
-            double[] c = new double[10];
+            const int arraysLenght = 10;
 
-            Console.WriteLine("O primeiro array tem os seguintes elementos: ");
-            foreach (double i in a) Console.Write($"{i}, ");
+            var a = new double[arraysLenght];
+            var b = new double[arraysLenght];
+            var c = new double[arraysLenght];
+
+            for (int i = 0; i < arraysLenght; i++) {
+                Console.WriteLine($"Informe o {i + 1}º número da primeira lista");
+                a[i] = Double.Parse(Console.ReadLine());
+            }
+
             Console.WriteLine("\n");
+
+            for (int i = 0; i < arraysLenght; i++) {
+                Console.WriteLine($"Informe o {i + 1} número da segunda lista");
+                b[i] = Double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nO primeiro array tem os seguintes elementos: ");
+            foreach (double i in a) Console.Write($"{i}, ");
+
+            Console.WriteLine("\n");
+
             Console.WriteLine("O segundo array tem os seguintes elementos: ");
             foreach (double i in b) Console.Write($"{i}, ");
+
             Console.Write("\n");
 
-            for (int i = 0; i < 10; i++) c[i] = a[i];
-            for (int j = 0; j < 10; j++) a[j] = b[j];
-            for (int k = 0; k < 10; k++) b[k] = c[k];
+            for (int i = 0; i < arraysLenght; i++) c[i] = a[i];
+            for (int j = 0; j < arraysLenght; j++) a[j] = b[j];
+            for (int k = 0; k < arraysLenght; k++) b[k] = c[k];
 
             Console.WriteLine("\nAgora o primeiro array tem os seguintes elementos: ");
             foreach (double i in a) Console.Write($"{i}, ");
+
             Console.WriteLine("\n");
+
             Console.WriteLine("Agora o segundo array tem os seguintes elementos: ");
             foreach (double i in b) Console.Write($"{i}, ");
+
             Console.Write("\n");
 
             Console.ReadLine();
