250b2828bbda0a63626316eb2919b8f4f6dd45ea
TATIANE <tatiane@tatianepedrelli.com.br>
2020-08-07 21:01:34 -0300

    exercise 4 fixed

--- Exercise4.cs
+++ Exercise4.cs
@@ -4,15 +4,24 @@ namespace arrays2 {
     class ExerciseFour {
         public static void Four(string[] args)
         {
-            Console.WriteLine("Exercício 4.\nDesenvolver um programa que efetue a leitura de cinco elementos de uma matriz");
-            Console.WriteLine("A do tipo vetor. No final, apresente o total da soma de todos os elementos que sejam impares");
+            Console.WriteLine("\nExercício 4.\nDesenvolver um programa que efetue a leitura de cinco elementos de uma matriz A do tipo vetor.");
+            Console.WriteLine("No final, apresente o total da soma de todos os elementos que sejam impares\n");
 
-            int[] a = new int[5] {45, 9, 14, 6, 29};
 
-            foreach (int value in a) Console.Write($"{value}, ");
+            const int arrayLenght = 5;
+
+            var x = new double[arrayLenght];
+
+            for (int i = 0; i < arrayLenght; i++) {
+                Console.WriteLine($"Informe o {i + 1}º número da matriz");
+                x[i] = Double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nA matriz contém os seguintes valores: ");
+            foreach (int value in x) Console.Write($"{value}, ");
 
             int sum = 0;
-            foreach(int value in a){
+            foreach(int value in x){
                 if (value % 2 != 0) sum += value;
             }
             Console.WriteLine($"\nA soma dos números ímpares do vetor é igual a {sum}");
